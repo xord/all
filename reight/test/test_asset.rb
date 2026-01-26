63f2f78fb410450c83df31acf1810dab857565bc
xordog <xordog@gmail.com>
2026-01-26 12:41:16 +0900

    Asset has name

--- reight/test/test_asset.rb
+++ reight/test/test_asset.rb
@@ -4,14 +4,17 @@ require_relative 'helper'
 class TestAsset < Test::Unit::TestCase
 
   def test_initialize()
-    assert_equal 1, asset(1, 2, 3, 4, 5, 6).id
-    assert_equal 2, asset(1, 2, 3, 4, 5, 6).w
-    assert_equal 3, asset(1, 2, 3, 4, 5, 6).h
-    assert_equal 4, asset(1, 2, 3, 4, 5, 6).x
-    assert_equal 5, asset(1, 2, 3, 4, 5, 6).y
-    assert_equal 6, asset(1, 2, 3, 4, 5, 6).value
-    assert_equal 0, asset(1, 2, 3)         .x
-    assert_equal 0, asset(1, 2, 3)         .y
+    assert_equal 1,            asset(1, 2, 3, 4, 5, 6, 'x').id
+    assert_equal 2,            asset(1, 2, 3, 4, 5, 6, 'x').w
+    assert_equal 3,            asset(1, 2, 3, 4, 5, 6, 'x').h
+    assert_equal 4,            asset(1, 2, 3, 4, 5, 6, 'x').x
+    assert_equal 0,            asset(1, 2, 3)              .x
+    assert_equal 5,            asset(1, 2, 3, 4, 5, 6, 'x').y
+    assert_equal 0,            asset(1, 2, 3)              .y
+    assert_equal [4, 5, 2, 3], asset(1, 2, 3, 4, 5, 6, 'x').frame
+    assert_equal 6,            asset(1, 2, 3, 4, 5, 6, 'x').value
+    assert_equal 'test_1',     asset(1, 2, 3, 4, 5, 6, nil).name
+    assert_equal 'x',          asset(1, 2, 3, 4, 5, 6, 'x').name
 
     assert_raise(ArgumentError) {R8::Asset.new( 1)}
     assert_raise(ArgumentError) {R8::Asset.new( 1,  2)}
@@ -32,59 +35,88 @@ class TestAsset < Test::Unit::TestCase
     assert_equal(
       { id: 1, w: 2, h: 3, x: 4,       value: 6},
       asset(1,    2,    3,    4,  nil,        6).save(proj))
+    assert_equal(
+      { id: 1, w: 2, h: 3, x: 4, y: 5, value: 6, name: 'x'},
+      asset(1,    2,    3,    4,    5,        6,       'x').save(proj))
+    assert_not_equal(
+      { id: 1, w: 2, h: 3, x: 4, y: 5, value: 6, name: nil},
+      asset(1,    2,    3,    4,    5,        6,       'x').save(proj))
   end
 
   def test_load()
-    a = Asset.load({id: 1, w: 2, h: 3, x: 4, y: 5, value: 6}, proj)
     assert_equal(
-      [  1,    2,   3,   4,   5,   6],
-      [a.id, a.w, a.h, a.x, a.y, a.value])
+      asset(              1,    2,    3,    4,    5,        6,       nil),
+      TestAsset.load({id: 1, w: 2, h: 3, x: 4, y: 5, value: 6}, proj))
+    assert_equal(
+      asset(              1,    2,    3,    4,    5,        6,       'x'),
+      TestAsset.load({id: 1, w: 2, h: 3, x: 4, y: 5, value: 6, name: 'x'}, proj))
   end
 
   def test_save_and_load()
-    a     = asset 1, 2, 3, 4, 5, 6
+    a     = asset 1, 2, 3, 4, 5, 6, 'x'
     state = a.save proj
-    assert_equal a, Asset.load(state, proj)
+    assert_equal a, TestAsset.load(state, proj)
   end
 
-  def test_frame()
-    assert_equal [4, 5, 2, 3], asset(1, 2, 3, 4, 5).frame
-  end
+  def test_modified?()
+    a      = TestAsset.new 0, 0, 1, 1
+    assert_true  a.modified?
 
-  def test_initial_modified?()
-    a = Asset.new 0, 0, 1, 1
-    assert_true  a                             .modified?
-    assert_false Asset.load(a.save(proj), proj).modified?
+    state  =     a.save(proj)
+    assert_false a.modified?
+    assert_false TestAsset.load(state, proj).modified? # the initial modified flag of a loaded asset
+
+    a.x    = 10
+    assert_true  a.modified?
+    a.save(proj)
+    assert_false a.modified?
+
+    a.y    = 20
+    assert_true  a.modified?
+    a.save(proj)
+    assert_false a.modified?
+
+    a.name = 'x'
+    assert_true  a.modified?
+    a.save(proj)
+    assert_false a.modified?
+
+    a.name = 'z'
+    assert_true  a.modified?
+    a.save(proj)
+    assert_false a.modified?
   end
 
   def test_compare_by_state_variables()
-    assert_equal(    asset(1, 2, 3, 4, 5, 6), asset(1, 2, 3, 4, 5, 6))
+    assert_equal(    asset(1, 2, 3, 4, 5, 6, 'x'), asset(1, 2, 3, 4, 5, 6, 'x'))
 
-    assert_not_equal(asset(1, 2, 3, 4, 5, 6), asset(0, 2, 3, 4, 5, 6))
-    assert_not_equal(asset(1, 2, 3, 4, 5, 6), asset(1, 9, 3, 4, 5, 6))
-    assert_not_equal(asset(1, 2, 3, 4, 5, 6), asset(1, 2, 9, 4, 5, 6))
-    assert_not_equal(asset(1, 2, 3, 4, 5, 6), asset(1, 2, 3, 0, 5, 6))
-    assert_not_equal(asset(1, 2, 3, 4, 5, 6), asset(1, 2, 3, 4, 0, 6))
-    assert_not_equal(asset(1, 2, 3, 4, 5, 6), asset(1, 2, 3, 4, 5, 0))
+    assert_not_equal(asset(1, 2, 3, 4, 5, 6, 'x'), asset(0, 2, 3, 4, 5, 6, 'x'))
+    assert_not_equal(asset(1, 2, 3, 4, 5, 6, 'x'), asset(1, 9, 3, 4, 5, 6, 'x'))
+    assert_not_equal(asset(1, 2, 3, 4, 5, 6, 'x'), asset(1, 2, 9, 4, 5, 6, 'x'))
+    assert_not_equal(asset(1, 2, 3, 4, 5, 6, 'x'), asset(1, 2, 3, 0, 5, 6, 'x'))
+    assert_not_equal(asset(1, 2, 3, 4, 5, 6, 'x'), asset(1, 2, 3, 4, 0, 6, 'x'))
+    assert_not_equal(asset(1, 2, 3, 4, 5, 6, 'x'), asset(1, 2, 3, 4, 5, 0, 'x'))
+    assert_not_equal(asset(1, 2, 3, 4, 5, 6, 'x'), asset(1, 2, 3, 4, 5, 0, nil))
+    assert_not_equal(asset(1, 2, 3, 4, 5, 6, 'x'), asset(1, 2, 3, 4, 5, 0, '_'))
   end
 
   private
 
   C = R8::CONTEXT__
 
-  class Asset < R8::Asset
-    def initialize(value = 0, *args, load: nil)
-      super(*args, load: load)
+  class TestAsset < R8::Asset
+    def initialize(value = 0, *args, load: nil, **kwargs)
+      super(*args, load: load, **kwargs)
       @value = load ? load[:state][:value] : value
     end
     attr_reader :value
     def      save(proj)           = super.merge value: @value
-    def self.load(state, project) = R8::Editable.load(Asset, state:, project:)
+    def self.load(state, project) = R8::Editable.load(TestAsset, state:, project:)
     def state_variables()         = super.merge value: @value
   end
 
-  def asset(id = 0, w = 8, h = 8, x = 0, y = 0, value = 100, **k) =
-    Asset.new(value, id, w, h, x, y, **k)
+  def asset(id = 0, w = 8, h = 8, x = 0, y = 0, value = 100, name = nil, **k) =
+    TestAsset.new(value, id, w, h, x, y, name: name, **k)
 
   def proj = R8::Project.new '/tmp'
 
