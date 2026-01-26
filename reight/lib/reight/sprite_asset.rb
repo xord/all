ef445a61d5d633f3682537f171cbe05eae2ee256
xordog <xordog@gmail.com>
2026-01-26 12:41:16 +0900

    Asset has name

--- reight/lib/reight/sprite_asset.rb
+++ reight/lib/reight/sprite_asset.rb
@@ -8,35 +8,33 @@ class Reight::SpriteAsset < Reight::Asset
     Reight::Editable.load Reight::SpriteAsset, state:, project:
   end
 
-  def initialize(*args, pos: nil, shape: nil, sensor: nil, anims: nil, load: nil)
-    super(*args, load: load)
+  def initialize(*args, name: nil, shape: nil, sensor: nil, anims: nil, load: nil)
+    super(*args, name: name, load: load)
     if load
-      state, project            = load.fetch_values :state, :project
-      pos, shape, sensor, anims = state.values_at :pos, :shape, :sensor, :anims
-      @pos         = pos&.then {C.create_vector(*_1)}
-      @anims       = anims&.transform_values {Reight::SpriteAnimation.load _1, project}
+      state, project       = load.fetch_values :state, :project
+      shape, sensor, anims = state.values_at :shape, :sensor, :anims
+      @anims               = anims&.map {Reight::SpriteAnimation.load _1, project}
       set_shape__  shape&.to_sym
       set_sensor__ sensor
     else
-      @pos, @anims = pos, anims
+      @anims = anims
       set_shape__  shape
       set_sensor__ sensor
     end
-    @anims ||= {}
+    @anims ||= []
   end
 
   def save(proj)
     super.tap {|h|
-      h[:pos]    = @pos.to_a(2) if @pos
-      h[:shape]  = @shape       if @shape
-      h[:sensor] = true         if sensor?
-      h[:anims]  = @anims.transform_values {_1.save proj} unless @anims.empty?
+      h[:shape]  = @shape                    if @shape
+      h[:sensor] = true                      if sensor?
+      h[:anims]  = @anims.map {_1.save proj} unless @anims.empty?
     }
   end
 
-  protected def state_variables() = super.merge(pos:, shape:, sensor:, anims:)
+  protected def state_variables() = super.merge(shape:, sensor:, anims:)
 
-  attr_reader :pos, :shape, :sensor, :anims
+  attr_reader :shape, :sensor, :anims
 
   def shape=(type)
     set_shape__ type
@@ -50,23 +48,23 @@ class Reight::SpriteAsset < Reight::Asset
 
   def sensor? = !!@sensor
 
-  def image   = @anims[:normal]&.image_at 0
+  def image   = @anims.first&.image_at 0
 
   def empty?()
     pixels__.all? {C.red(_1) == 0 && C.green(_1) == 0 && C.blue(_1) == 0}
   end
 
   def with(**kwargs)
-    #kwargs => {id:, w:, h:, x:, y:, pos:, shape:, sensor:, anims:}
-    id, width, height, w, h, x, y, pos, shape, sensor, anims =
-      kwargs.values_at :id, :width, :height, :w, :h, :x, :y, :pos, :shape, :sensor, :anims
+    #kwargs => {id:, w:, h:, x:, y:, shape:, sensor:, anims:}
+    id, width, height, w, h, x, y, name, shape, sensor, anims =
+      kwargs.values_at :id, :width, :height, :w, :h, :x, :y, :name, :shape, :sensor, :anims
     self.class.new(
       id          || @id,# TODO: fix duplicated id
       width  || w || @width,
       height || h || @height,
       x           || @x,
       y           || @y,
-      pos:    kwargs.key?(:pos)    ? pos    : @pos,
+      name:   kwargs.key?(:name)   ? name   : @name,
       shape:  kwargs.key?(:shape)  ? shape  : @shape,
       sensor: kwargs.key?(:sensor) ? sensor : @sensor,
       anims:  kwargs.key?(:anims)  ? anims  : @anims)# fix parent
@@ -83,7 +81,6 @@ class Reight::SpriteAsset < Reight::Asset
       0, 0, w, h, chip: self,
       image: image, offset: [x, y], shape: shape, physics: physics
     ).tap do |sp|
-      sp.x, sp.y = pos.x, pos.y if pos
       if physics
         sp.sensor = true if sensor?
         sp.fix_angle
