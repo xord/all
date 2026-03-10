3ec9652817bc9a6527d422a05a0614dba24c9aa5
xordog <xordog@gmail.com>
2026-03-10 15:12:07 +0900

    Add KeyMap prototype

--- reight/lib/reight/app/script/interface.rb
+++ reight/lib/reight/app/script/interface.rb
@@ -5,7 +5,9 @@ class Reight::ScriptEditorInterface < Reight::ViewController
   def initialize(editor)
     super
 
-    e = editor
+    e       = editor
+    @keymap = Reight::ScriptEditor::KeyMap.new e, text_editor
+
     e.script_changed {text_editor.text = _1.text}
 
     e.disable_history do
@@ -32,23 +34,128 @@ class Reight::ScriptEditorInterface < Reight::ViewController
   end
 
   def key_pressed(pressings)
-    #shift, ctrl, cmd = [SHIFT, CONTROL, COMMAND].map {pressings.include? _1}
-    e = @editor
-    text_editor.cursors.each do |c|
-      case C.key_code
-      when ENTER     then e.replace_text(*c.selection,   "\n")
-      when DELETE    then e.replace_text(*c.selection( 1), '')
-      when BACKSPACE then e.replace_text(*c.selection(-1), '')
-      when ESC       then text_editor.cursor = text_editor.cursor
-      when UP        then c.row -= 1
-      when DOWN      then c.row += 1
-      when LEFT      then c.col -= 1
-      when RIGHT     then c.col += 1
-      else e.replace_text(*c.selection, C.key) if C.key&.match?(/^[[:print:]]+$/)
-      end
-    end
-    text_editor.redraw_cursors
-  rescue Reight::Text::NoLineError
+    @keymap.key_pressed C.key, C.key_code, pressings
   end
 
 end# ScriptEditorInterface
+
+
+class Reight::ScriptEditor::KeyMap
+
+  def initialize(editor, text_editor)
+    @editor, @text_editor = editor, text_editor
+  end
+
+  def self.commands()
+    @commands ||= {}
+  end
+
+  def self.command(name, desc: nil, &block)
+    commands[name] = {desc:, block:}
+  end
+
+  command :replace_text, desc: <<~DOC do |str, cursor:, **|
+    insert or replace text
+  DOC
+    @editor.replace_text(*cursor.selection, str)
+  end
+
+  command :delete_char_forward do |cursor:, **|
+    @editor.replace_text(*cursor.selection( 1), '')
+  end
+
+  command :delete_char_backward do |cursor:, **|
+    @editor.replace_text(*cursor.selection(-1), '')
+  end
+
+  command :move_char_forward do |cursor:, **|
+    cursor.col += 1
+  end
+
+  command :move_char_backward do |cursor:, **|
+    cursor.col -= 1
+  end
+
+  command :move_line_forward do |cursor:, **|
+    cursor.row += 1
+  end
+
+  command :move_line_backward do |cursor:, **|
+    cursor.row -= 1
+  end
+
+  command :move_to_line_head do |cursor:, **|
+    cursor.col = 0
+  end
+
+  command :move_to_line_end do |cursor:, text:, **|
+    cursor.col = text[cursor.row].size
+  end
+
+  command :cut_to_line_end do |cursor:, text:, **|
+    size = text[cursor.row].size - cursor.col
+    size = 1 if size == 0
+    @editor.replace_text cursor.index, size, ''
+  end
+
+  command :cursor_clear do
+    @text_editor.cursor = @text_editor.cursor
+  end
+
+  def self.keymap()
+    @keymap ||= {}
+  end
+
+  def self.map(key, command, *args)
+    keymap[Set.new [key].flatten] = [command, args]
+  end
+
+  map :enter,     :replace_text, "\n"
+  map :tab,       :replace_text, "\t"
+  map :backspace, :delete_char_backward
+  map :delete,    :delete_char_forward
+  map :left,      :move_char_backward
+  map :right,     :move_char_forward
+  map :up,        :move_line_backward
+  map :down,      :move_line_forward
+  map :home,      :move_to_line_head
+  map :end,       :move_to_line_end
+  map :escape,    :cursor_clear
+
+  map [:m, :control], :replace_text, "\n"
+  map [:i, :control], :replace_text, "\t"
+  map [:b, :control], :move_char_backward
+  map [:f, :control], :move_char_forward
+  map [:p, :control], :move_line_backward
+  map [:n, :control], :move_line_forward
+  map [:a, :control], :move_to_line_head
+  map [:e, :control], :move_to_line_end
+  map [:h, :control], :delete_char_backward
+  map [:d, :control], :delete_char_forward
+  map [:k, :control], :cut_to_line_end
+
+  def do(command, *args)
+    block = self.class.commands[command]&.fetch(:block, nil) || return
+    @text_editor.cursors[0, 1].each do |cursor|
+      instance_exec *args, editor: @editor, text: @text_editor.text, cursor:, &block
+    end
+  end
+
+  def key_pressed(key, code, pressings)
+    mods          = [:shift, :control, :command].select {pressings.include? _1}
+    keymap        = self.class.keymap
+    command, args = [[key, *mods], [code, *mods], [key], [code]]
+      .lazy.filter_map {keymap[Set.new _1]}.first
+
+    if command
+      self.do command, *args
+    elsif key&.match?(/^[[:print:]]+$/)
+      self.do :replace_text, key
+    else
+      return
+    end
+    @text_editor.redraw_cursors
+  rescue Reight::Text::NoLineError
+  end
+
+end# KeyMap
